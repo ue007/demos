f036356c9dfb2720d9d9e8d9e288620ec6c9dd49
ue007 <ue007@webgpu.info>
2024-04-05 22:10:47 +0800

    feat: add hello-fabic-react

---
+++ miscellaneous/hello-react-fabric/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import FabricExample from "./FabricExample";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fabric with React</h1>
+      <FabricExample />
+    </div>
+  );
+}
